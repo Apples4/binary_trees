3fb8651b4c7dfc4ab40333b8e78d8b98553e62c2
Apples4 <nhlanhla.mavuka@gmail.com>
2024-01-05 00:42:31 +0200

    changes Q4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_is_leaf - know if a node is a leaf
  * does not have any child neither left nor right
@@ -7,5 +8,5 @@
  */
 int binary_tree_is_leaf(const binary_tree *node)
 {
-	return ((!node || node->left || node->right) ? 0 : 1)	
+	return ((!node || node->left || node->right) ? 0 : 1);	
 }
